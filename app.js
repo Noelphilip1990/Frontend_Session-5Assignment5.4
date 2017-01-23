4b072c89685406477b2b4d5f3b645373527424ec
Noelphilip1990 <noyo1000gt@gmail.com>
2017-01-23 12:16:27 +0530

    Create app.js
    js File of array of objects

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+var employee1 = {name:"John", age:25, salary:"25000",address:{city:"cochin",state:"kerala",pincode:"682025"}};
+var employee2= {name:"Jill", age:26, salary:"25000",address:{city:"banglore",state:"karnataka",pincode:"582024"}};
+var employee3 = {name:"Jack", age:27, salary:"25000",address:{city:"chennai",state:"Tamil nadu",pincode:"892024"}};
+var employee4 = {name:"Jose", age:29, salary:"25000",address:{city:"cochin",state:"kerala",pincode:"252027"}};
+var employee5 = {name:"James", age:30, salary:"25000",address:{city:"cochin",state:"kerala",pincode:"682024"}};
+var employeeArray = [employee1,employee2,employee3,employee4,employee5];
